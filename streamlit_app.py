0f33a170d9071ff03f52097aca04a6c67552b420
lmalakalapalli <lmalakal@charlotte.edu>
2024-06-28 17:48:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.title("Data App Assignment, on June 20th")
 
 # Load the dataset
-df = pd.read_csv("Superstore_Sales_utf8.csv", parse_dates=True)
+df = pd.read_csv("Superstore_Sales_utf8.csv")
 
 # Display the dataframe
 st.dataframe(df)
@@ -12,5 +12,6 @@ st.dataframe(df)
 # Display the column names
 st.write("### DataFrame Columns")
 st.write(df.columns)
+
 
 
